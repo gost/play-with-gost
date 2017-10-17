9909ef16c4a42df6c86b1e518a5321ba65066b92
bert <btemme@hotmail.com>
2017-10-17 16:06:39 +0200

    trying out in console

---
+++ app/pwg.console/Program.cs
@@ -0,0 +1,25 @@
+﻿using Ductus.FluentDocker.Services;
+using System;
+using System.Linq;
+using Ductus.FluentDocker.Commands;
+
+namespace pwg.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Environment.SetEnvironmentVariable("PORT", "8080");
+            Environment.SetEnvironmentVariable("VIRTUAL_HOST", "gost_prod.lvh.me");
+
+            var file = "docker-compose.yml";
+            var hosts = new Hosts().Discover();
+            var _docker = hosts.FirstOrDefault(x => x.IsNative) ?? hosts.FirstOrDefault(x => x.Name == "default");
+            var containers = _docker.Host.ComposeUp(composeFile: file, altProjectName: "gost_prod");
+
+            Console.WriteLine("Docker-compose fired: " + containers.Success);
+            Console.ReadKey();
+        }
+    }
+}
